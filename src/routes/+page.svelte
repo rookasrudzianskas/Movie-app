497dd3e95f3b0b1f75de51ae561832815404a7ca
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 20:00:11 +0200

    new

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({fetch}) {
+        // this will wait to receive the data, but as server side rendering
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
+        const data = await response.json();
+        console.log("This is data:", data);
+
+        if(response.ok) {
+            return {
+                props: {
+                    popular: data.results
+                }
+            };
+        }
+    }
+</script>
+
+<script>
+
+</script>
