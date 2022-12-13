b528298fdcb0ce74ae62a61f8000012b650b42b1
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 19:40:15 +0200

    Initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let count = 0;
+
+    function incrementButton() {
+        count++;
+    }
+</script>
+
+<h1>Number: {count}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/about">Go to about</a>
+<button on:click={incrementButton}>Increment</button>
+
+<style>
+    p {
+        font-size: 2rem;
+    }
+</style>
