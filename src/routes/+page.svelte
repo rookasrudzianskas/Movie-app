008f8b3ee57df842212087708b5fdfd364b17d7b
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 20:19:44 +0200

    Rokas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,25 @@
 <script context="module">
-    export async function load({fetch}) {
+    export async function load({}) {
         // this will wait to receive the data, but as server side rendering
-        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
-        const data = await response.json();
-        console.log("This is data:", data);
-
-        if(response.ok) {
-            return {
-                props: {
-                    popular: data.results
-                }
-            };
-        }
+        console.log("fetching data");
+        // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
+        // const data = await response.json();
+        // console.log("This is data:", data);
+        //
+        // if(response.ok) {
+        //     return {
+        //         props: {
+        //             popular: data.results
+        //         }
+        //     };
+        // }
     }
 </script>
 
 <script>
 
 </script>
+
+<h1>About this website</h1>
+<p>TODO...</p>
+<a href="/">Home</a>
