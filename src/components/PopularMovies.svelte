b79f9c430a68014a40df31d9d822f0024f91ae4d
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 09:55:45 +0200

    Add movie card styles
    The movie card was missing some styles.
    
    - Added a description div
    - Added a style block
    - Added styles for the description div
    - Added styles for the movie card

--- src/components/PopularMovies.svelte
+++ src/components/PopularMovies.svelte
@@ -35,6 +35,70 @@
             video: false,
             vote_average: 7.3,
             vote_count: 2743,
+        },
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436272,
+            original_language: "en",
+            original_title: "Rokas Black",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Rokas Black",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
+        },
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436272,
+            original_language: "en",
+            original_title: "Rokas Black",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Rokas Black",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
+        },
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436272,
+            original_language: "en",
+            original_title: "Rokas Black",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Rokas Black",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
+        },
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436272,
+            original_language: "en",
+            original_title: "Rokas Black",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Rokas Black",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
         }
     ]
 </script>
@@ -45,3 +109,15 @@
         <MovieCard {movie} />
     {/each}
 </div>
+
+<style>
+    h3 {
+        padding: 0 1rem;
+    }
+    .popular-movies {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        grid-column-gap: 1rem;
+        grid-row-gap: 1rem;
+    }
+</style>
