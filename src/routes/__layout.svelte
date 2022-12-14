2b268f4b30281cd27a80231135212129ff6be3cd
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 10:38:29 +0200

    Add layout
    This commit adds a layout to the application.
    
    The layout contains a navigation bar.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import global from '../global.css'
+</script>
+
+<nav>
+    <ul>
+        <li>Hello</li>
+    </ul>
+</nav>
+<slot />
--- src/routes/movie/[id]/+page.svelte
+++ src/routes/movie/[id]/+page.svelte
@@ -2,7 +2,7 @@
     import {onMount} from "svelte";
     export let movieDetail;
     onMount( async() => {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/436270?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/436270?api_key=&language=en-US&page=1`);
         const data = await response.json();
 
         if(response.ok) {
