83daa17278d8c07cf44f49f743d1b35db81d9b37
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 08:58:58 +0200

    Add PopularMovies component
    This component will be used to display the popular movies.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,31 @@
-<script context="module">
-    export async function load({}) {
-        // this will wait to receive the data, but as server side rendering
-        console.log("fetching data");
-        // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
-        // const data = await response.json();
-        // console.log("This is data:", data);
-        //
-        // if(response.ok) {
-        //     return {
-        //         props: {
-        //             popular: data.results
-        //         }
-        //     };
-        // }
-    }
-</script>
+<!--<script context="module">-->
+<!--    // export async function load({}) {-->
+<!--        // this will wait to receive the data, but as server side rendering-->
+<!--        // console.log("fetching data");-->
+<!--        // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);-->
+<!--        // const data = await response.json();-->
+<!--        // console.log("This is data:", data);-->
+<!--        //-->
+<!--        // if(response.ok) {-->
+<!--        //     return {-->
+<!--        //         props: {-->
+<!--        //             popular: data.results-->
+<!--        //         }-->
+<!--        //     };-->
+<!--        // }-->
+<!--    // }-->
+<!--</script>-->
 
 <script>
+    import { onMount } from 'svelte';
+    export let movies;
+    onMount(async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=3d8757bd718142f06b4285fd3344456d&language=en-US&page=1`);
+        const data = await response.json();
+
+        if (response.ok) {
+            movies = data;
+        }
+    })
 
 </script>
-
-<h1>About this website</h1>
-<p>TODO...</p>
-<a href="/">Home</a>
