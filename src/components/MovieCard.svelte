edbc90933ade11ff42370111c0c8ecd56fd3efc5
Rokas <rookas.rudzenskas@gmail.com>
2022-12-14 09:22:54 +0200

    Add MovieCard component
    This component will be used to display a single movie.

---
+++ src/components/MovieCard.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let movie;
+</script>
+
+<h1>{movie.title}</h1>
+
--- src/components/PopularMovies.svelte
+++ src/components/PopularMovies.svelte
@@ -1,11 +1,47 @@
 <script>
+    import MovieCard from "./MovieCard.svelte";
+
     export let movies;
-    console.log(movies);
+
+    movies = [
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436270,
+            original_language: "en",
+            original_title: "Black Adam",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Black Adam",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
+        },
+        {
+            adult: false,
+            backdrop_path: "/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+            genre_ids: (3) [28, 14, 878],
+            id:436272,
+            original_language: "en",
+            original_title: "Rokas Black",
+            overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+            popularity: 5382.38,
+            poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+            release_date: "2022-10-19",
+            title: "Rokas Black",
+            video: false,
+            vote_average: 7.3,
+            vote_count: 2743,
+        }
+    ]
 </script>
 
 <h3>Popular Movies</h3>
 <div class="popular-movies">
-    {#each movies.results as movie}
-        <h1>{movie.results}</h1>
+    {#each movies as movie}
+        <MovieCard {movie} />
     {/each}
 </div>
